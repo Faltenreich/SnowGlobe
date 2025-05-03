b426b9e94b0a74ad8747bf35279f81640e2fd370
Faltenreich <philipp.fahlteich@gmail.com>
2025-05-03 12:56:28 +0200

    Setup dependency injection

--- androidApp/src/main/java/com/faltenreich/snowglobe/MainActivity.kt
+++ androidApp/src/main/java/com/faltenreich/snowglobe/MainActivity.kt
@@ -1,17 +1,24 @@
-package com.faltenreich.snowglobe.android
+package com.faltenreich.snowglobe
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.material3.*
 import com.faltenreich.snowglobe.AppView
+import org.koin.compose.KoinContext
 
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        enableEdgeToEdge()
+
         setContent {
-            AppView()
+            KoinContext {
+                AppView()
+            }
         }
     }
 }
---
+++ shared/src/commonMain/kotlin/com/faltenreich/snowglobe/AppModule.kt
@@ -0,0 +1,5 @@
+package com.faltenreich.snowglobe
+
+import org.koin.dsl.module
+
+fun appModule() = module {}
