ffa4f7c1c0d2654e5bf8d5bd6c5ac0001895b857
Faltenreich <philipp.fahlteich@gmail.com>
2025-05-03 17:32:19 +0200

    Merge GlobeState

--- shared/src/commonMain/kotlin/com/faltenreich/snowglobe/globe/GlobeState.kt
+++ shared/src/commonMain/kotlin/com/faltenreich/snowglobe/globe/GlobeState.kt
@@ -1,5 +1,21 @@
 package com.faltenreich.snowglobe.globe
 
+import androidx.compose.ui.geometry.Size
 import com.faltenreich.snowglobe.globe.snowflake.SnowFlakeState
+import com.faltenreich.snowglobe.sensor.SensorData
 
-data class GlobeState(val snowFlakes: List<SnowFlakeState>)
+data class GlobeState(
+    val canvas: Size,
+    val sensorData: SensorData,
+    val snowFlakes: List<SnowFlakeState>,
+) {
+
+    companion object {
+
+        val Initial = GlobeState(
+            canvas = Size.Zero,
+            sensorData = SensorData.Zero,
+            snowFlakes = emptyList(),
+        )
+    }
+}
--- shared/src/commonMain/kotlin/com/faltenreich/snowglobe/globe/GlobeViewModel.kt
+++ shared/src/commonMain/kotlin/com/faltenreich/snowglobe/globe/GlobeViewModel.kt
@@ -9,8 +9,7 @@ import com.faltenreich.snowglobe.sensor.SensorProvider
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.firstOrNull
-import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
@@ -18,43 +17,35 @@ import kotlin.time.Duration.Companion.seconds
 
 class GlobeViewModel(private val sensorProvider: SensorProvider) : ViewModel() {
 
-    private val sensorData = sensorProvider.data
-    private val canvas = MutableStateFlow(Size.Zero)
-    private val snowFlakes = MutableStateFlow(emptyList<SnowFlakeState>())
-
-    val state = snowFlakes.map(::GlobeState).stateIn(
+    private val _state = MutableStateFlow(GlobeState.Initial)
+    val state = _state.stateIn(
         scope = viewModelScope,
         started = SharingStarted.Lazily,
-        initialValue = GlobeState(snowFlakes = emptyList()),
+        initialValue = GlobeState.Initial,
     )
 
     init {
         viewModelScope.launch {
-            snowFlakes.update {
-                (0 until 100).map { index ->
-                    SnowFlakeState(
-                        coordinates = Rect(
-                            left = index.toFloat(),
-                            top = index.toFloat(),
-                            right = index.toFloat() + 20,
-                            bottom = index.toFloat() + 20,
-                        )
-                    )
+            sensorProvider.data.collectLatest { sensorData ->
+                _state.update { state ->
+                    state.copy(sensorData = sensorData)
                 }
             }
         }
         viewModelScope.launch {
             while (true) {
-                val sensorData = sensorData.firstOrNull()
-                println("Sensor data: $sensorData")
-                snowFlakes.update { snowFlakes ->
-                    snowFlakes.map { snowFlake ->
-                        snowFlake.copy(
-                            coordinates = snowFlake.coordinates.copy(
-                                top = snowFlake.coordinates.top + 10,
-                            ),
-                        )
-                    }
+                _state.update { state ->
+                    state.copy(
+                        snowFlakes = state.snowFlakes.map { snowFlake ->
+                            snowFlake.copy(
+                                coordinates = snowFlake.coordinates.copy(
+                                    top = snowFlake.coordinates.top + (state.sensorData.y * 10),
+                                    left = snowFlake.coordinates.top + (state.sensorData.x * 10),
+                                ),
+                            )
+                        }
+                    )
+
                 }
                 delay(1.seconds)
             }
@@ -69,7 +60,26 @@ class GlobeViewModel(private val sensorProvider: SensorProvider) : ViewModel() {
         sensorProvider.stop()
     }
 
-    fun setCanvas(size: Size) {
-        canvas.update { size }
+    fun setCanvas(canvas: Size) {
+        val x = canvas.width / 2f
+        val y = canvas.height / 2f
+        val width = 20f
+        val height = 20f
+
+        _state.update { state ->
+            state.copy(
+                canvas = canvas,
+                snowFlakes = listOf(
+                    SnowFlakeState(
+                        coordinates = Rect(
+                            left = x,
+                            top = y,
+                            right = x + width,
+                            bottom = y + height,
+                        )
+                    )
+                )
+            )
+        }
     }
 }
