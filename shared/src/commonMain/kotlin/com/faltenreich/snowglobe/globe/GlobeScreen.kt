7b28c174ee7def3eccc37efd1205e57f90b24153
Faltenreich <philipp.fahlteich@gmail.com>
2025-05-15 21:59:36 +0200

    Extend layout

--- shared/src/commonMain/kotlin/com/faltenreich/snowglobe/globe/GlobeScreen.kt
+++ shared/src/commonMain/kotlin/com/faltenreich/snowglobe/globe/GlobeScreen.kt
@@ -2,7 +2,8 @@ package com.faltenreich.snowglobe.globe
 
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.gestures.detectTapGestures
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -12,12 +13,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.toSize
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.faltenreich.snowglobe.globe.debug.GlobeDebugInfo
@@ -39,63 +40,58 @@ fun GlobeScreen(
     LaunchedEffect(Unit) { viewModel.start() }
     DisposableEffect(Unit) { onDispose { viewModel.stop() } }
 
-    Box(modifier = modifier) {
-        Canvas(
-            modifier = Modifier
-                .fillMaxSize()
-                .onGloballyPositioned { coordinates ->
-                    viewModel.prepare(coordinates.size.toSize())
-                    viewModel.run()
-                }
-                .pointerInput(Unit) {
-                    detectTapGestures(onTap = viewModel::touch)
-                },
-        ) {
-            if (state.showDebugInfo) {
-                state.grid.cells.flatten().forEach { cell ->
-                    drawLine(
-                        color = color,
-                        start = cell.rectangle.topLeft,
-                        end = cell.rectangle.topRight,
-                    )
-                    drawLine(
-                        color = color,
-                        start = cell.rectangle.topRight,
-                        end = cell.rectangle.bottomRight,
-                    )
+    Scaffold(
+        modifier = modifier,
+        bottomBar = {
+            if (state.showUi) {
+                BottomAppBar {
+                    // TODO
                 }
             }
-
-            state.grid.snowFlakes.forEach { snowFlake ->
-                path.addOval(snowFlake.rectangle)
-            }
-            drawPath(path, color = color)
-            path.reset()
-        }
-
-        if (state.showUi) {
-            Scaffold(
-                containerColor = Color.Transparent,
-                bottomBar = {
-                    BottomAppBar {
-
+        },
+    ) { padding ->
+        Column {
+            Canvas(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(if (state.showUi) padding else PaddingValues(all = 0.dp))
+                    .onGloballyPositioned { coordinates ->
+                        viewModel.prepare(coordinates.size.toSize())
+                        viewModel.run()
                     }
-                },
-            ) { padding ->
-                Box(
-                    modifier = Modifier
-                        .padding(padding)
-                        .fillMaxSize(),
-                    contentAlignment = Alignment.BottomCenter,
-                ) {
-                    if (state.showDebugInfo) {
-                        GlobeDebugInfo(
-                            state = state,
-                            modifier = Modifier.fillMaxWidth(),
+                    .pointerInput(Unit) {
+                        detectTapGestures(onTap = viewModel::touch)
+                    },
+            ) {
+                if (state.showDebugInfo) {
+                    state.grid.cells.flatten().forEach { cell ->
+                        drawLine(
+                            color = color,
+                            start = cell.rectangle.topLeft,
+                            end = cell.rectangle.topRight,
+                        )
+                        drawLine(
+                            color = color,
+                            start = cell.rectangle.topRight,
+                            end = cell.rectangle.bottomRight,
                         )
                     }
                 }
+
+                state.grid.snowFlakes.forEach { snowFlake ->
+                    path.addOval(snowFlake.rectangle)
+                }
+                drawPath(path, color = color)
+                path.reset()
             }
+        }
+
+        // TODO: Place somewhere
+        if (state.showDebugInfo) {
+            GlobeDebugInfo(
+                state = state,
+                modifier = Modifier.fillMaxWidth(),
+            )
         }
     }
 }
